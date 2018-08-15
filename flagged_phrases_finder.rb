3aa8149e1218f772f13cc75fc8693d21e086ea47
sweetyclem <pirlot.clementine@gmail.com>
2018-08-15 09:11:02 +0200

    calculate score for one file at a time

--- flagged_phrases_finder.rb
+++ flagged_phrases_finder.rb
@@ -1,14 +1,34 @@
+
+
+# Get flagged phrases and put them in arrays
 high_risk_phrases = []
 low_risk_phrases = []
-# Get flagged phrases and put them in arrays
+
 File.open("high_risk_phrases.txt").each do |line|
   high_risk_phrases.push(line.strip)
 end
+
 File.open("low_risk_phrases.txt").each do |line|
   low_risk_phrases.push(line.strip)
 end
 
+# Get file name and read file content
+file_name = ARGV.first
 file = ARGV.first
 input = File.read(file)
-puts input
-score = 0
+
+# Find out how many occurrences of each phrase the text contains
+nb_high_risk = 0
+nb_low_risk = 0
+
+high_risk_phrases.each do |phrase|
+  nb_high_risk += 1 if input.downcase.include?(phrase.downcase)
+end
+
+low_risk_phrases.each do |phrase|
+  nb_low_risk += 1 if input.downcase.include?(phrase.downcase)
+end
+
+# Calculate and save score to a text file
+score = (nb_low_risk) + (nb_high_risk * 2)
+File.open("offensive_scores.txt", "a") {|f| f.write("#{file_name}:#{score}") }
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Flagged phrases finder
 
-This ruby command line program takes a file name as an argument, and outputs an offensive score to a text file named offensive_score.txt
+This ruby command line program takes a file name as an argument, and outputs an offensive score to a text file named offensive_scores.txt
 
 ## Start the program
 ```
